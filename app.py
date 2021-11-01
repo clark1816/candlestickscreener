bc842c3f7460c41029436373ac6f80de8825c779
clark1816 <33646776+clark1816@users.noreply.github.com>
2021-11-01 14:48:20 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,63 @@
+import os, csv
+from pandas.core.frame import DataFrame
+import yfinance as yf
+import pandas as pd
+from flask import Flask, render_template, request
+from patterns import patterns
+import talib
+# from fastapi import FastAPI, Request
+# from fastapi.templating import Jinja2Templates
+
+# app2 = FastAPI()
+
+# templates = Jinja2Templates(directory="E:/programmin/NewScreenerApp/templates")
+
+app = Flask(__name__)
+
+# @app2.get("/tradingview_widget")
+# async def tradingview_widget(request: Request):
+#     return templates.TemplateResponse("tradingview.html", {"request": request})
+
+@app.route("/")
+def index():
+    pattern  = request.args.get('pattern', None)
+    stocks = {}
+
+    with open('dataset/compaines.csv') as f:
+        for row in csv.reader(f):
+            stocks[row[0]] = {'company': row[1]}
+    print(stocks)
+    if pattern:
+        datafiles = os.listdir('dataset/daily')
+        for filename in datafiles:
+            df=pd.read_csv('dataset/daily/{}'.format(filename))
+            
+            pattern_function = getattr(talib, pattern)
+
+            symbol = filename.split('.')[0]
+
+            try:
+                result = pattern_function(df['Open'], df['High'], df['Low'], df['Close'])
+                last = result.tail(1).values[0]
+                if last > 0:
+                    stocks[symbol][pattern] = 'bullish'
+                elif last < 0:
+                    stocks[symbol] [pattern] = 'bearish'
+                else:
+                     stocks[symbol] [pattern] = None
+            except:
+                pass
+
+    return render_template('index.html', patterns=patterns, stocks = stocks, current_pattern=pattern)
+
+@app.route('/snapshot')
+def snapshot():
+    with open('dataset/compaines.csv') as f:
+        companies = f.read().splitlines()
+        for company in companies:
+            symbol = (company.split(',')[0])
+            df = yf.download(symbol, start="2021-09-01", end="2021-11-1")
+            df.to_csv('dataset/daily/{}.csv'.format(symbol))
+    return{
+        'code': 'succes'
+    }
---
+++ stockwebapp.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+import cufflinks as cf
+import datetime
+import webbrowser
+
+# App title
+st.markdown('''
+# Stock Price App
+Shown are the stock price data for query companies!
+
+**Credits**
+- Built in `Python` using `streamlit`,`yfinance`, `cufflinks`, `pandas` and `datetime`
+''')
+st.write('---')
+
+# Sidebar
+st.sidebar.subheader('Query parameters')
+url = 'http://127.0.0.1:5000/'
+if st.sidebar.button('Candle Stick Screener'):
+    webbrowser.open_new_tab(url)
+start_date = st.sidebar.date_input("Start date", datetime.date(2021, 1, 1))
+end_date = st.sidebar.date_input("End date", datetime.date(2021, 10, 1))
+
+
+# Retrieving tickers data
+ticker_list = pd.read_csv('https://raw.githubusercontent.com/shilewenuw/get_all_tickers/master/get_all_tickers/tickers.csv')
+tickerSymbol = st.sidebar.selectbox('Stock ticker', ticker_list) # Select ticker symbol
+tickerData = yf.Ticker(tickerSymbol) # Get ticker data
+tickerDf = tickerData.history(period='1d', start=start_date, end=end_date) #get the historical prices for this ticker
+
+
+
+# Ticker information
+string_logo = '<img src=%s>' % tickerData.info['logo_url']
+st.markdown(string_logo, unsafe_allow_html=True)
+
+string_name = tickerData.info['longName']
+st.header('**%s**' % string_name)
+
+string_summary = tickerData.info['longBusinessSummary']
+st.info(string_summary)
+
+# Ticker data
+st.header('**Ticker data**')
+st.write(tickerDf)
+
+# Bollinger bands
+st.header('**Bollinger Bands**')
+qf=cf.QuantFig(tickerDf,title='First Quant Figure',legend='top',name='GS')
+qf.add_bollinger_bands()
+fig = qf.iplot(asFigure=True)
+st.plotly_chart(fig)
+
+####
+#st.write('---')
+#st.write(tickerData.info)
