0deb4cb2975e356518c1ac3c48f5da6ed985cc88
clark1816 <clark.mcquiston@gmail.com>
2021-12-09 09:16:32 -0500

    updated prices

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def snapshot():
         companies = f.read().splitlines()
         for company in companies:
             symbol = (company.split(',')[0])
-            df = yf.download(symbol, start="2021-10-01", end="2021-12-2")
+            df = yf.download(symbol, start="2021-10-01", end="2021-12-10")
             df.to_csv('dataset/daily/{}.csv'.format(symbol))
     return{
         'code': 'succes'
